864ae412c1600f08548ef972746148a7b98aa1cd
Barakar13 <yuz165@ucsd.edu>
2022-05-09 13:11:06 -0700

    adding a test file

--- skillDemon.java
+++ skillDemon.java
@@ -1,3 +1,7 @@
 public class skillDemon{
 
+    public static int division(int a, int b) {
+        return a/b;
+    }
+
 }
---
+++ skillDemonTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class skillDemonTest {
+    @Test
+    public void divisionTest() {
+        assertEquals(1, skillDemon.division(4, 2));
+
+
+    
+}
+}
